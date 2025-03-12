4745cf6df7a6e7c3beae74f713bf8c3da153174d
Fi Dunnington <fi.dunnington@gmail.com>
2025-03-12 14:52:07 +0100

    animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,76 @@
-const observer = new IntersectionObserver(entries => {
- entries.forEach(entry =>{
-  if(entry.isIntersecting){
-    document.querySelectorAll(".animated")[0].classList.add("fadeInLeft")
-    document.querySelectorAll(".animated")[0].classList.add("fadeInBottom")
-  }
- })
-})
 
-observer.observe(document.querySelector(".container"))
+ 
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+
+  /**
+   * Preloader
+   */
+  /* const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  } */
+
+  /**
+   * Mobile nav toggle
+   */
+    /* cconst mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+    /* cdocument.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+
+
+  /**
+   * Animation on scroll function and init
+   */
+function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
